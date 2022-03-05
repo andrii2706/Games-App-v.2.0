3b84d935a48c96c3226903ff3ba63c71af7483b2
andrii2706 <83228054+andrii2706@users.noreply.github.com>
2022-03-05 18:33:13 +0200

    topscrollbutton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {ViewportScroller} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  @HostListener('window:scroll', ['$event']) onScroll(event: any){
+    let pageYoffset = window.pageYOffset;
+  }
+
+  constructor(private scroll: ViewportScroller) { }
+
+  ngOnInit(): void{
+
+  }
+
+  showOrHideButton(){
+    const position  = window.pageYOffset;
+    if (position === 0){
+
+    }else if(position >= 500){
+
+    }
+  }
+
+  scrollToTop(){
+    this.scroll.scrollToPosition([0,0]);
+    this.showOrHideButton()
+  }
 }
